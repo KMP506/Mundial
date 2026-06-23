a5db3c75cb940a63fbf5dea358c27ffc608dff1e
Kevin <kevinmejias866@gmail.com>
2026-06-23 15:16:26 -0600

    Se desarrolla clase mundial
    Se agregaron metodos, constructor y override en la clase mundial

--- src/mundiales/Mundial.java
+++ src/mundiales/Mundial.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package mundiales;
-
+import java.time.LocalDate;
+import selecciones.seleccion;
+import partidos.Partido;
 /**
  *
  * @author kevin
@@ -11,22 +13,50 @@ package mundiales;
 
 public class Mundial {
     protected String nombre;
-    protected int año;
+    protected LocalDate año;
+    protected String seleccion;
+    protected Partido partidos;
+    protected Partido partidosResultado;
     protected String pais;
     
     public String getNombre(){
         return nombre;
     }
-    public int getAño(){
+    public LocalDate getAño(){
         return año;
     }
     public String getPais(){
         return pais;
     }
     
-    public Mundial(String nombre,int año, String pais){
+    public Partido getPartidos(){
+        return partidos;
+    }
+    
+    public Partido getPartidosResultado(){
+        return partidosResultado;
+    }
+    
+   public String getSeleccion(){
+       return seleccion;
+   }
+   
+   public void setAño(LocalDate año){
+       this.año=año;
+   }
+   
+    public Mundial(String nombre,LocalDate año, String pais,seleccion Nomseleccion, Partido partidos, Partido partidosResultado){
         this.nombre=nombre;
         this.año=año;
         this.pais=pais;
+        this.seleccion=Nomseleccion.getNombre();
+        this.partidos=partidos;
+        this.partidosResultado=partidosResultado;
+        
+    }
+    
+    @Override
+    public String toString() {
+        return "Mundial{" + "nombre=" + nombre + ", año=" + año + ", pais=" + pais + ", seleccion=" + seleccion+ ", equipo A=" + partidos + "equipo B= "+partidosResultado ;
     }
 }
